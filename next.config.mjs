401675297edb8726a384e19a7f639b63319dad4e
Rachel Nurmi <rnurmi@lazparking.com>
2024-11-12 14:08:02 -0500

    Update configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  devIndicators: {
+    appIsrStatus: true, // Change to false if needed
+    buildActivity: true, // Change to false if needed
+    buildActivityPosition: "bottom-right", // Options: 'bottom-right', 'bottom-left', 'top-right', 'top-left'
+  },
+};
 
 export default nextConfig;
