63933b3e6ef63acd29e4533f79f5a3dc68f5fb39
Rachel Nurmi <rnurmi@lazparking.com>
2024-11-15 14:22:31 -0500

    Remove depriciated mongodb props

--- database/index.js
+++ database/index.js
@@ -14,8 +14,6 @@ export const connectToDatabase = async () => {
   try {
     await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI, {
       dbName: "jennifersGift",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
 
     isConnected = true;
