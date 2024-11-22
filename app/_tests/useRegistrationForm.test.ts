be5ba061924ef6370f7e786c0d3f27668989c818
Rachel Nurmi <rnurmi@lazparking.com>
2024-11-22 10:26:30 -0500

    Set up Jest dom and react testing library

--- app/_tests/useRegistrationForm.test.ts
+++ app/_tests/useRegistrationForm.test.ts
@@ -1,8 +1,23 @@
-import { useRegistrationForm } from "@app/register/hooks/useRegistrationForm";
+import { useRegistrationForm } from "../register/hooks/useRegistrationForm";
 import { renderHook, act } from '@testing-library/react';
 
 describe('useRegistrationForm test', () => {
-  it('', ()=> {
+  it('Initialize with default values', ()=> {
+    const { result } = renderHook(() => useRegistrationForm())
 
+    expect(result.current.formData.firstName).toBe("");
+    expect(result.current.formData.lastName).toBe("");
+    expect(result.current.formData.email).toBe("");
+    expect(result.current.formData.phone).toBe("");
+    expect(result.current.formData.selection.attendAsGolfer).toBe(false);
+    expect(result.current.formData.selection.attendAsTeam).toBe(false);
+    expect(result.current.formData.selection.attendDinner).toBe(false);
+    expect(result.current.formData.selection.sponsorHole).toBe(false);
+    expect(result.current.formData.selection.sponsorDoubleHole).toBe(false);
+    expect(result.current.formData.selection.sponsorBeverage).toBe(false);
+    expect(result.current.formData.selection.sponsorLunch).toBe(false);
+    expect(result.current.formData.selection.sponsorDinner).toBe(false);
+    expect(result.current.formData.paymentType).toBe("venmo");
+    expect(result.current.total).toBe(0);
   })
 })
--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,11 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jest-environment-jsdom', // Explicitly use the installed jsdom environment
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+  }
 };
 
 export default config;
