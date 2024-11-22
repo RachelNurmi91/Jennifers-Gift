d3228dff47da1677fd927fb0815979e2b24f7eec
Rachel Nurmi <rnurmi@lazparking.com>
2024-11-22 10:17:08 -0500

    Add React Testing Library

---
+++ app/_tests/useRegistrationForm.test.ts
@@ -0,0 +1,8 @@
+import { useRegistrationForm } from "@app/register/hooks/useRegistrationForm";
+import { renderHook, act } from '@testing-library/react';
+
+describe('useRegistrationForm test', () => {
+  it('', ()=> {
+
+  })
+})
